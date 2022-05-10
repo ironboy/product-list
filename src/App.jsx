10552cc892bd2e87ea72b045c16c9cf8e1adebfd
Ironboy <ironboy@users.noreply.github.com>
2022-05-10 13:19:14 +0200

    all products have a category property (a getter)

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ import ProductEdit from './ProductEdit';
 
 const { Product, Categorie: Category } = factory;
 
+window.Product = Product
+
 export default function App() {
 
   let s = useStates('main', {
@@ -29,6 +31,25 @@ export default function App() {
       // get the categories from the db
       s.categories = await Category.find();
     })();
+
+    // All objects created by a certain class
+    // share a common template object called prototype
+
+    // Object.defineProperties can add properties to an object
+    // if you use it on a prototype you will add a property
+    // to all objects sharing that prototype
+    Object.defineProperties(Product.prototype, {
+      category: {
+        configurable: true,
+        get() {
+          // this = the product we are 
+          // asking for the category property
+          return s.categories.find(category =>
+            category.id === this.categoryId);
+        }
+      }
+    });
+
   }, []);
 
   return s.products.length ? <Router>
